42bdb4e91593670a522cc9111813bdd411a7f720
Suhui Choi <shc0411@g.yju.ac.kr>
2026-04-06 21:17:51 +0900

    02-variables

---
+++ 02-variables/p02.py
@@ -0,0 +1,28 @@
+"""
+실습 2: 변수의 자료형 확인하기
+
+아래 변수들의 자료형을 type()으로 확인하고 출력하세요.
+
+[지시사항]
+각 변수와 자료형을 함께 출력하세요.
+"""
+
+a = "파이썬"
+b = 2026
+c = 3.14
+d = False
+
+# 아래에 코드를 작성하세요
+
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+print(d, type(d))
+
+"""
+[실행 결과]
+파이썬 <class 'str'>
+2026 <class 'int'>
+3.14 <class 'float'>
+False <class 'bool'>
+"""
