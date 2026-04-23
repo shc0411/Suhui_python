cd1f3936acdfa0fa42d2d0a8d0da301f54e4350f
Suhui Choi <shc0411@g.yju.ac.kr>
2026-04-23 22:36:10 +0900

    for 연습

---
+++ 00-practice/kurikasu.py
@@ -0,0 +1,16 @@
+# for i in range(5):
+#     print("i =", i)
+
+# print("반복 끝")
+
+word = "python"
+for ch in word:
+    print(ch)
+
+nums = [10, 20, 30]
+for n in nums:
+    print(n)
+
+colors = ("R", "G", "B")
+for c in colors:
+    print(c)
